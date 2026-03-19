20c9b6eb0be84331497e9c448e6178352b27385a
mimaizumi <katsuw000000@gmail.com>
2026-03-20 02:04:44 +0900

    second commit.

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 # 各種ライブラリの読み込み
 import streamlit as st
 from dotenv import load_dotenv
-from langchain-openai import ChatOpenAI
+from langchain.chat_models import ChatOpenAI
 from langchain.schema import SystemMessage, HumanMessage
+
 
 # 環境変数の読み込み
 load_dotenv()
